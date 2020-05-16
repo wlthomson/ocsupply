f80a54ad73ac8d2bd301eb1aebc2136b1454a5ca
josh <griffinjoshua5@gmail.com>
2020-05-16 13:19:24 +1200

    Add Item and Query schemas

---
+++ src/schema/Item.ts
@@ -0,0 +1,14 @@
+import { gql } from "apollo-server";
+
+export const ItemSchema = gql`
+  """
+  A generic Item record. For example a medicine like Ibuprofen, or a Sach, like a Nut Sach
+  """
+  type Item {
+    "The name of an Item"
+    name: String
+
+    "The code of an item"
+    code: String
+  }
+`;
