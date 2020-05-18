149a8cd5151c3286315b5b75b3eee598e76114d9
josh <griffinjoshua5@gmail.com>
2020-05-19 09:09:56 +1200

    Add simple express server and create-react-app

---
+++ web/index.js
@@ -0,0 +1,16 @@
+import Express from "express";
+import Path from "path";
+
+const PORT = 4000;
+const MESSAGE = `
+\#####################################################\n
+🥳🥳🥳 Server started @ http://localhost:${PORT} 🥳🥳🥳\n
+#####################################################`;
+
+const app = new Express();
+
+app.use(Express.static(Path.join(__dirname, "client", "build")));
+app.get("/", (_, res) =>
+  res.sendFile(Path.join(__dirname, "client", "build", "index.html"))
+);
+app.listen(PORT, () => console.log(MESSAGE));
