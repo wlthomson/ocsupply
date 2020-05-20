0f6b775c2fce207c0285c568f6339a7bde152593
josh <griffinjoshua5@gmail.com>
2020-05-20 12:26:38 +1200

    Add process args

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { typeDefs, resolvers } from "./schema";
-
 import { ApolloServer } from "apollo-server";
 
-new ApolloServer({ typeDefs, resolvers }).listen().then(({ url }) =>
+const PORT = process.argv[2] || 4000;
+
+new ApolloServer({ typeDefs, resolvers }).listen(PORT).then(({ url }) =>
   // tslint:disable-next-line:no-console
   console.log(
     `#####################################################\n🥳🥳🥳 Server started @ ${url} 🥳🥳🥳\n#####################################################`
