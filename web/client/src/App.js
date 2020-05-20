ccc1fa99093fa276e79436a064b25ecf6c7701e3
josh <griffinjoshua5@gmail.com>
2020-05-20 12:26:45 +1200

    Add text field to input a port

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -2,19 +2,39 @@ import React from "react";
 import "./App.css";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
+import TextField from "@material-ui/core/TextField";
 import { Requisitions } from "./Requisitions";
 import { NewRequisitionButton } from "./NewRequisitionButton";
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000",
-});
-
 function App() {
+  const [graphqlUri, setGraphqlUri] = React.useState("http://localhost:4000");
+  const [apolloClient, setApolloClient] = React.useState(
+    new ApolloClient({ uri: graphqlUri })
+  );
+
+  const setNewClient = React.useCallback((e) => {
+    setGraphqlUri(e.target.value);
+    setApolloClient(new ApolloClient({ uri: e.target.value }));
+  });
+
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apolloClient}>
       <div className="App">
-        <NewRequisitionButton />
-        <Requisitions />
+        <div
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <TextField
+            label={"GraphQL Server Uri:"}
+            onChange={setNewClient}
+            value={graphqlUri}
+          />
+          <NewRequisitionButton />
+          <Requisitions />
+        </div>
       </div>
     </ApolloProvider>
   );
