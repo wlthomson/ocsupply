4bf430f630b3a3e1b30cdb0329870646cbc03928
Will Thomson <will.tho@outlook.com>
2020-05-21 22:07:56 +1200

    Tidy up sync script

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,51 +1,44 @@
 /**
  * sync.js
  * 
- * Basic proof-of-concept for primary-to-satellite syncing using CouchDB.
+ * Simulates primary-to-satellite sync by populating primary server replicator database.
  */
 
 const fs = require('fs');
-const { put, post } = require('./http')
 
-const main = async () => {
+const forEach = async (arr, fn) => {
+    arr.reduce(async (promise, curr) => {
+        await promise;
+        fn(curr);
+    }, Promise.resolve());
+}
+
+const sync = async () => {
+    // Create satellite site databases.
     const hosts = JSON.parse(fs.readFileSync('hosts.json', 'utf8'));
+    const satellites = hosts.filter(host => host.site !== 'site_p');
+    forEach(satellites, async satellite => {
+        const { hostname, port, auth } = satellite;
+        const nano = require('nano')(`http://${auth}@${hostname}:${port}`);
+        const dbs = await nano.db.list();
+        if (dbs.includes('ocsupply')) await nano.db.destroy('ocsupply');
+        if (dbs.includes('_replicator')) await nano.db.destroy('_replicator');
+        await nano.db.create('ocsupply');
+        await nano.db.create('_replicator');
+    });
+    
+    // Insert sync documents to primary replicator.
     const sites =  JSON.parse(fs.readFileSync('sites.json', 'utf8'));
     const stores = JSON.parse(fs.readFileSync('stores.json', 'utf8'));
 
-    // Create satellite site databases.
-    const satellites = hosts.filter(host => host.site !== 'site_p');
-
-    satellites.reduce(async (promise, satellite) => {
-        await promise; 
-
-        const { hostname, port, auth } = satellite;
-        const nano = require('nano')(`http://${auth}@${hostname}:${port}`);
-        
-        const dbs = await nano.db.list();
-
-        if (dbs.includes('ocsupply')) await nano.db.destroy('ocsupply');
-        if (dbs.includes('_replicator')) await nano.db.destroy('_replicator');
-
-        await nano.db.create('ocsupply');
-        await nano.db.create('_replicator');
-    }, Promise.resolve());
-    
-    // Insert sync documents to primary replicator.
     const { hostname, port, auth } = hosts.find(host => host.site === 'site_p');
     const nano = require('nano')(`http://${auth}@${hostname}:${port}`);
 
-    satellites.reduce(async (outerPromise, satellite) => {
-        await outerPromise;
+    forEach(satellites, async satellite => {
         const { auth, site: satelliteSite } = satellite;
         const site = sites.find(site => site.id === satelliteSite);
         const { stores: activeStores } = site;
-        activeStores.reduce(async (innerPromise, activeStore) => {
-            await innerPromise;
-            const options =  { selector: { _id: activeStore } } 
-            await nano.db.replicate('ocsupply', `http://${auth}@${satelliteSite}:5984/ocsupply`, options);
-        }, Promise.resolve())
-        activeStores.reduce(async (innerPromise, storeId) => {
-            await innerPromise;
+        forEach(activeStores, async storeId => {
             const { 
                 request_requisitions: requestRequisitionIds = [],
                 response_requisitions: responseRequisitionIds = [],
@@ -54,10 +47,8 @@ const main = async () => {
             const syncIds = [storeId, ...requestRequisitionIds, ...responseRequisitionIds, ...itemIds ];
             const options = { selector: { "_id": { "$in": syncIds } } };
             await nano.db.replicate('ocsupply', `http://${auth}@${satelliteSite}:5984/ocsupply`, options);
-        }, Promise.resolve())
-    }, Promise.resolve());
-
-
+        })
+    });
 }
 
-main();
+sync();
