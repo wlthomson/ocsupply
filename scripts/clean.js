2ca8a18522ab31e68cf895c8f2f8d096f9ecf761
Will Thomson <will.tho@outlook.com>
2020-05-21 22:07:22 +1200

    Add clean up script

---
+++ scripts/clean.js
@@ -0,0 +1,22 @@
+/**
+ * clean.js
+ * 
+ * Removes demo network and sites. Run when done or reinitialising an existing demo.
+ */
+
+const fs = require('fs');
+const { spawnSync } = require('child_process');
+
+const clean = () => {
+    // Remove demo sites.
+    const hosts = JSON.parse(fs.readFileSync('hosts.json', 'utf8'));
+    const containers = hosts.map(host => host.site)
+    spawnSync('docker', ['rm', '-f', ...containers]);
+
+    // Remove demo network.
+    const network = 'susnet';
+    spawnSync('docker', ['network', 'rm', network]);
+    
+}
+
+clean();
