f4024f48eb1ce4b01e2e91fde05609364b8e49d9
Will Thomson <will.tho@outlook.com>
2020-05-22 13:26:16 +1200

    Update schema classes

--- src/Database/Requisition/IRequisition.ts
+++ src/Database/Requisition/IRequisition.ts
@@ -1,7 +1,12 @@
 import Nano from "nano";
+import { IRequisitionLine } from "./IRequisitionLine";
 
 export interface IRequisition extends Nano.MaybeDocument {
   _id: string;
-  storeId: string;
+  number: number;
+  fromStoreId: string;
+  toStoreId: string;
+  requestRequisitionId: string;
+  lines: [IRequisitionLine]
   type: string;
 }
--- src/Database/Store/IStore.ts
+++ src/Database/Store/IStore.ts
@@ -3,5 +3,9 @@ import Nano from "nano";
 export interface IStore extends Nano.MaybeDocument {
   _id: string;
   name: string;
+  code: string;
+  itemIds: [string];
+  requestRequisitionIds: [string];
+  responseRequisitionIds: [string];
   type: string;
 }
